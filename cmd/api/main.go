2cffc3fe88f166e6f98116e8560ff75c370f06c5
Ishank Gupta <ishank195@gmail.com>
2026-08-17 14:16:30 +0530

    add config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
+
+	"github.com/ishankgupta95/olx-api/internal/config"
 )
 
 func main() {
+
+	cfg := config.MustLoad()
+	fmt.Println("Starting olx server...")
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("GET /healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -17,13 +23,14 @@ func main() {
 	})
 
 	srv := http.Server{
-		Addr:         ":8090",
+		Addr:         ":" + cfg.Port,
 		Handler:      mux,
 		ReadTimeout:  time.Second * 10,
 		WriteTimeout: time.Second * 30,
 		IdleTimeout:  time.Second * 60,
 	}
 
+	log.Printf("server is listening on %s", cfg.Port)
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("server failed %v", err)
 	}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ishankgupta95/olx-api
 
 go 1.26.4
+
+require github.com/joho/godotenv v1.5.1
